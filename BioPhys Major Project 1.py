1c73efb743d73276b3c4233fe6a370c4d3e1ac2c
Ross Dewberry <ross.dewberry@hotmail.com>
2019-05-14 13:16:05 -0700

    Comments and End define

--- BioPhys Major Project 1.py
+++ BioPhys Major Project 1.py
@@ -1,6 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# α ratio of people interacting with other communities: should be from 0.0-1.0: define as 1/r where r is distance from two communities
+# β risk of getting infected: if 1.0 100% of contractoin needs to be between 0.0-1.0
+# γ rate of recovery of infected person
+# μ death and birth rate
+# N population from data
+
 # Comm 1
 β =
 γ =
@@ -12,6 +18,7 @@ N =
 S =
 I =
 V = #are there those vaccinated before the diease starts
+End = 1000
 
 dS = (1-p)*N*μ-μ*S-β*S*(I/N + α*I/N)
 dI = β*S*(I/N) - γ*(I/N)
